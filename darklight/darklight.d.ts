5e4a7430843b7eb37dd841783b93f0ca884080c6
boyninja1555 <flappyfloorg@gmail.com>
2026-03-23 15:11:36 -0500

    Moved function types to global scope Using Window broke when I wanted shorthand syntax like `$()` and not `window.$()`

--- darklight/darklight.d.ts
+++ darklight/darklight.d.ts
@@ -1,27 +1,25 @@
 export { }
 declare global {
-    interface Window {
-        /**
-         * Gets the currently active theme. See `window.loadTheme()` for loading from local storage.
-         * @returns First active theme found (dark/light)
-         */
-        getTheme(): "dark" | "light" | null
+    /**
+     * Gets the currently active theme. See `window.loadTheme()` for loading from local storage.
+     * @returns First active theme found (dark/light)
+     */
+    function getTheme(): "dark" | "light" | null
 
-        /**
-         * Sets the currently active theme. See `window.saveTheme()` for saving to local storage.
-         * @param {"dark" | "light"} theme Theme to activate (dark/light)
-         */
-        setTheme(theme: "dark" | "light"): void
+    /**
+     * Sets the currently active theme. See `window.saveTheme()` for saving to local storage.
+     * @param {"dark" | "light"} theme Theme to activate (dark/light)
+     */
+    function setTheme(theme: "dark" | "light"): void
 
-        /**
-         * Loads the stored theme from local storage. See `window.getTheme()` for getting the currently active theme.  
-         * Does not return anything. Instead uses `window.setTheme("theme")`
-         */
-        loadTheme(): void
+    /**
+     * Loads the stored theme from local storage. See `window.getTheme()` for getting the currently active theme.  
+     * Does not return anything. Instead uses `window.setTheme("theme")`
+     */
+    function loadTheme(): void
 
-        /**
-         * Saves the currently active theme from `window.getTheme()` to local storage. Load with `window.loadTheme()`
-         */
-        saveTheme(): void
-    }
+    /**
+     * Saves the currently active theme from `window.getTheme()` to local storage. Load with `window.loadTheme()`
+     */
+    function saveTheme(): void
 }
--- floorquery/floorquery.d.ts
+++ floorquery/floorquery.d.ts
@@ -1,21 +1,19 @@
 export { }
 declare global {
-    interface Window {
-        /**
-         * ### INTERNAL HELPER
-         * 
-         * Returns Error, used when a query-related problem happens. Fill in operator and params correctly for best debugging experience.
-         * @param message Log message
-         * @param operator Operator shown in debug section
-         * @param params Params shown in debug section
-         */
-        queryError(message: string, operator: string, params: string): void
+    /**
+     * ### INTERNAL HELPER
+     * 
+     * Returns Error, used when a query-related problem happens. Fill in operator and params correctly for best debugging experience.
+     * @param message Log message
+     * @param operator Operator shown in debug section
+     * @param params Params shown in debug section
+     */
+    function queryError(message: string, operator: string, params: string): void
 
-        /**
-         * Shorthand syntax for many operations. Provided by FloorJS.
-         * @param operator Typed operator which uses params
-         * @param params Extra info optionally used by operator
-         */
-        $(operator: "el" | "EL" | "ls" | "LS" | "ss" | "SS", params: string): any
-    }
+    /**
+     * Shorthand syntax for many operations. Provided by FloorJS.
+     * @param operator Typed operator which uses params
+     * @param params Extra info optionally used by operator
+     */
+    function $(operator: "el" | "EL" | "ls" | "LS" | "ss" | "SS", params: string): any
 }

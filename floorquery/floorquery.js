89a28ef3a6129680388b68871ffa6c4ee1e5cb56
Floor Mann <flappyfloorg@gmail.com>
2026-03-23 01:02:41 -0500

    Fix error handling for operator end in query

--- floorquery/floorquery.js
+++ floorquery/floorquery.js
@@ -3,7 +3,7 @@ const operatorEnd = "]"
 window.$ = query => {
     if (query.length < operatorLength + 1) throw new Error(`Attempted query without an operator and ${operatorEnd}`)
     let params = query.slice(operatorLength + 1)
-    if (query[3] !== operatorEnd) throw new Error(`Attempted query without a ${operatorEnd} after the operator`)
+    if (query[operatorLength] !== operatorEnd) throw new Error(`Attempted query without a ${operatorEnd} after the operator`)
     switch (query.slice(0, operatorLength)) {
         case "el":
             return document.querySelector(params)

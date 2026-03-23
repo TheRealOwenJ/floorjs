463c6c0839c258be4925b81048b304eb030c9883
boyninja1555 <flappyfloorg@gmail.com>
2026-03-23 01:09:34 -0500

    Better error handling for invalid queireis

--- floorquery/floorquery.js
+++ floorquery/floorquery.js
@@ -1,15 +1,15 @@
 const operatorLength = 2
 const operatorEnd = "]"
 window.$ = query => {
-    if (query.length < operatorLength + 1) throw new Error(`Attempted query without an operator and ${operatorEnd}`)
+    if (query.length < operatorLength + 1) throw new Error(`Attempted query without an operator and ${operatorEnd}\n${query}`)
     let params = query.slice(operatorLength + 1)
-    if (query[operatorLength] !== operatorEnd) throw new Error(`Attempted query without a ${operatorEnd} after the operator`)
+    if (query[operatorLength] !== operatorEnd) throw new Error(`Attempted query without a ${operatorEnd} after the operator\n${query}`)
     switch (query.slice(0, operatorLength)) {
         case "el":
             return document.querySelector(params)
         case "EL":
             return document.querySelectorAll(params)
         default:
-            throw new Error("Attempted query with invalid operator")
+            throw new Error(`Attempted query with invalid operator!\n${query}`)
     }
 }
